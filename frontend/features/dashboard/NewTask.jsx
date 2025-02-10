2f4ebde771d95b6bea993f0de6715cc4d3a994b2
Akash Kumar <akashkumar59999@gmail.com>
2025-02-10 20:03:32 +0530

    little change in task model

--- frontend/features/dashboard/NewTask.jsx
+++ frontend/features/dashboard/NewTask.jsx
@@ -4,7 +4,7 @@ function NewTask({ onClose }) {
   return (
     <div className="fixed inset-0 flex items-end justify-end overflow-hidden right-10">
       <motion.div
-        className="bg-gray-800 text-white p-4 rounded-xl shadow-lg w-96"
+        className="bg-[#2a2b2d] text-white p-4 rounded-xl shadow-lg w-96"
         initial={{ y: 100, opacity: 0 }} // Start off-screen (below)
         animate={{ y: 0, opacity: 1 }} // Slide up into view
         exit={{ y: 100, opacity: 0 }} // Slide down when closing
@@ -22,20 +22,22 @@ function NewTask({ onClose }) {
         <div className="space-y-3">
           <input
             type="text"
-            placeholder="Task title"
-            className="w-full p-2 bg-gray-700 rounded text-white outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Task name"
+            className="w-full p-2 transparent font-semibold rounded text-2xl text-white outline-none"
           />
           <div className="flex gap-2">
-            <select className="w-1/2 p-2 bg-gray-700 rounded text-white outline-none focus:ring-2 focus:ring-blue-500">
+            <div className="p-2">For</div>
+            <select className="w-1/2 p-2  rounded text-white outline-none ">
               <option>Assignee</option>
             </select>
-            <select className="w-1/2 p-2 bg-gray-700 rounded text-white outline-none focus:ring-2 focus:ring-blue-500">
+            <div className="p-2">in</div>
+            <select className="w-1/2 p-2 rounded text-white outline-none ">
               <option>Project</option>
             </select>
           </div>
           <textarea
             placeholder="Task description"
-            className="w-full p-2 bg-gray-700 rounded text-white outline-none resize-none focus:ring-2 focus:ring-blue-500"
+            className="w-full p-2 transparent rounded text-white outline-none resize-none"
             rows="3"
           />
         </div>
