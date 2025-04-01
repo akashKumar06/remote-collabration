70d08700946e5d3be822df3a86f9c806c81195a3
Akash Kumar <akashkumar59999@gmail.com>
2025-04-01 20:00:49 +0530

    dashboard page

--- frontend/src/features/dashboard/project/ProjectDasboard.jsx
+++ frontend/src/features/dashboard/project/ProjectDasboard.jsx
@@ -1,8 +1,18 @@
 function ProjectDasboard() {
   return (
     <div>
-      <div>kuch aayega ok yha pe ana pdega kuch vaishnavi</div>
-      <div>hmm..</div>
+      <div className="flex gap-4 justify-center mt-2">
+        <div className="w-36 h-36 border rounded p-2">Completed tasks</div>
+        <div className="w-36 h-36 border rounded p-2">Incomplete tasks</div>
+        <div className="w-36 h-36 border rounded p-2">Overdue tasks</div>
+        <div className="w-36 h-36 border rounded p-2">total tasks</div>
+      </div>
+      <div className="flex mt-5 flex-wrap gap-6">
+        <div className="w-96 h-96 border rounded"></div>
+        <div className="w-96 h-96 border rounded"></div>
+        <div className="w-96 h-96 border rounded"></div>
+        <div className="w-96 h-96 border rounded"></div>
+      </div>
     </div>
   );
 }
