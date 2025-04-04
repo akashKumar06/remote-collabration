b9ac4aa213068893818c6cb94008c3bf5bdffe2c
Vaishnavi Sisodiya <vaishvi.sisodiya28@gmail.com>
2025-04-04 17:37:25 +0530

    file page

--- frontend/src/features/dashboard/project/Files.jsx
+++ frontend/src/features/dashboard/project/Files.jsx
@@ -1,5 +1,13 @@
 function Files() {
-  return <div>Files</div>;
+  return( <div className="mt-10">
+            <div className="w-30 h-20 flex-col items-center justify-center ml-140">
+              <img className="rounded-xl" src="https://sdmntprwestus.oaiusercontent.com/files/00000000-81d0-5230-ad67-ee7a31cbd44e/raw?se=2025-04-04T12%3A58%3A47Z&sp=r&sv=2024-08-04&sr=b&scid=d7cd274d-1194-53b2-8c3b-d4a8aaf1ff62&skoid=acefdf70-07fd-4bd5-a167-a4a9b137d163&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-04-04T09%3A07%3A59Z&ske=2025-04-05T09%3A07%3A59Z&sks=b&skv=2024-08-04&sig=GSww0Mrp9p171rFIwE38XqNYFsC4pnfq8byiRxkBH5s%3D" alt=""></img>
+            </div>
+            <div className="mt-20 text-center">
+                <p>All attachments to tasks & messages in this project will appear here</p>
+            </div>
+       </div>
+  );
 }
 
 export default Files;
