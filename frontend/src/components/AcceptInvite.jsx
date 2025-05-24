90e3c007d3d9a5573cbbf23923cb9b3f2da9d17c
Vaishnavi Sisodiya <vaishvi.sisodiya28@gmail.com>
2025-05-24 14:11:46 +0530

    change

--- frontend/src/components/AcceptInvite.jsx
+++ frontend/src/components/AcceptInvite.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router";
 import axios from "axios";
 import { UserIcon } from "@heroicons/react/24/solid";
 
